462c7fdc95cd4004502e189b09c868bc173686e2
Name <sampsonJemison@gmail.com>
2019-11-17 00:26:39 -0500

    fixed the comments

--- life_logger.py
+++ life_logger.py
@@ -64,12 +64,15 @@ xml
 
 # A definition is the text following space after the type name.
 
-# Get a value from the user.
-# Returns it along with label for the value.
-# A value is a int from a to b inclusive.
-# Signature in config: value (a,b) label
-# str -> (label: str, value: int)
 def user_enter_value(definition):
+    """
+    Get a value from the user.
+    Returns it along with label for the value.
+    A value is a int from a to b inclusive.
+    Signature in config: value (a,b) label
+    str -> (label: str, value: int)
+    """
+
     label = get_value_label(definition)
     range = get_value_range(definition)
     a = range[0]
@@ -89,53 +92,74 @@ def user_enter_value(definition):
 
     return (label, value)
 
-# Prompts the user to enter a value response.
-# nothing -> str
+
 def user_enter_value_response():
+    """
+    Prompts the user to enter a value response.
+    nothing -> str
+    """
     return input('Value: ')
 
-# Given a value definition, returns its label.
-# str -> str
+
 def get_value_label(definition):
+    """
+    Given a value definition, returns its label.
+    str -> str    
+    """
     return definition.split(' ')[1]
 
-# Given a value definition, returns its range.
-# str -> (int, int)
+
 def get_value_range(definition):
+    """
+    Given a value definition, returns its range.
+    str -> (int, int)    
+    """
     range = definition.split(' ')[0]
     values = range.split(',')
     a = values[0][1:]
     b = values[1][:-1]
     return (int(a), int(b))
 
-# Given a user response and a range of numbers,
-# Returns if the response in a int and that in falls in range of [a, b)
-# str, number, number -> bool
+
 def is_valid_value(response, a, b):
+    """
+    Given a user response and a range of numbers,
+    Returns if the response in a int and that in falls in range of [a, b).
+    str, number, number -> bool    
+    """
     if response.isdigit():
         value = get_value_from_response(response)
         return value >= a and value < b
     return False
 
-# Returns true if a boolean response is valid.
-# str -> int
+
 def get_value_from_response(response):
+    """
+    Returns true if a boolean response is valid.
+    str -> int    
+    """
     return int(response)
 
-# Get a time from the user.
-# Returns it along with label for the value.
-# A time is a float.
-# Signature in config: time label
-# str -> (label: str, value: float)
+
 def user_enter_time(definition):
+    """
+    Get a time from the user.
+    Returns it along with label for the value.
+    A time is a float.
+    Signature in config: time label
+    str -> (label: str, value: float)    
+    """
     print("time " + definition)
 
-# Gets a did_do from the user.
-# Returns it along with label for the value.
-# A did_do is a bool.
-# Signature in config: did_do label
-# str -> (label: str, value: bool)
+
 def user_enter_did_do(definition):
+    """
+    Gets a did_do from the user.
+    Returns it along with label for the value.
+    A did_do is a bool.
+    Signature in config: did_do label
+    str -> (label: str, value: bool)    
+    """
 
     label = get_did_do_label(definition)
 
@@ -151,53 +175,77 @@ def user_enter_did_do(definition):
 
     return (label, bool_response)
 
-# Prompts the user to enter a boolean response and returns its value in lower case.
-# nothing -> str
+
 def user_enter_boolean_response():
+    """
+    Prompts the user to enter a boolean response and returns its value in lower case.
+    nothing -> str    
+    """
     return input('(y/n)').lower()
     
-# Given a valid did do, returns its label.
-# str -> str
+
 def get_did_do_label(definition):
+    """
+    Given a valid did do, returns its label.
+    str -> str    
+    """
     return definition
 
-# Given a valid bool response, returns the response in boolean form.
-# str -> bool
+
 def get_bool_from_response(response):
+    """
+    Given a valid bool response, returns the response in boolean form.
+    str -> bool    
+    """
     return response == 'y'
 
-# Returns true if a boolean response is valid.
-# str -> bool
+
 def is_valid_boolean_response(response):
+    """
+    Returns true if a boolean response is valid.
+    str -> bool    
+    """
     return response == 'n' or response == 'y'
 
-# Gets a note from the user.
-# Returns it along with label for the value.
-# A note is a str.
-# Signature in config: note label
-# str -> (label: str, value: str)
+
 def user_enter_note(definition):
+    """
+    Gets a note from the user.
+    Returns it along with label for the value.
+    A note is a str.
+    Signature in config: note label
+    str -> (label: str, value: str)    
+    """
     print("get note " + definition)
 
-# Gets a key_event from the user.
-# Returns it along with label for the value.
-# A key_event is a str.
-# Signature in config: key_event label
-# str -> (label: str, value: str)
+
 def user_enter_key_event(definition):
+    """
+    Gets a key_event from the user.
+    Returns it along with label for the value.
+    A key_event is a str.
+    Signature in config: key_event label
+    str -> (label: str, value: str)    
+    """
     print("key event " + definition)
 
-# Gets a state_change from the user.
-# Returns it along with label for the value.
-# A state_change is a str.
-# Signature in config: state_change label
-# str -> (label: str, value: str)
+
 def user_enter_state_change(definition):
+    """
+    Gets a state_change from the user.
+    Returns it along with label for the value.
+    A state_change is a str.
+    Signature in config: state_change label
+    str -> (label: str, value: str)    
+    """
     print("state change " + definition)
 
-# Takes in the data for a config and returns a list of functions to call the meet it's criteria.
-# list of str -> list of (none -> str)
+
 def config_to_functions(config):
+    """
+    Takes in the data for a config and returns a list of functions to call the meet it's criteria.
+    list of str -> list of (none -> str)    
+    """
     type_to_input_functions = get_type_map()
 
     functions = []
@@ -220,45 +268,66 @@ def config_to_functions(config):
 
     return functions
 
-# Returns the command type.
-# str -> str
+
 def get_command_type(line):
+    """
+    Returns the command type.
+    str -> str    
+    """
     line = line.split(' ', 1)
     return line[0]
 
-# Returns the command definition.
-# str -> str
+
 def get_command_definition(line):
+    """
+    Returns the command definition.
+    str -> str    
+    """
     line = line.split(' ', 1)
     return line[1]
 
-# Returns true if the given line was commented out.
-# str -> bool
+
 def is_line_commented(line):
+    """
+    Returns true if the given line was commented out.
+    str -> bool    
+    """
     return len(line) >= 2 and line[0:2] == constants.COMMENT_OUT_STRING
 
-# Given a function and it's perameter returns a function object.
-# (str -> anything), str -> (none -> anything)
+
 def function_maker(input_func, perameter):
+    """
+    Given a function and it's perameter returns a function object.
+    (str -> anything), str -> (none -> anything)    
+    """
     return lambda: input_func(perameter)
 
-# Checks that a given type is valid.
-# If not, throw an exception.
-# str -> none or error
+
 def check_type(type):
+    """
+    Checks that a given type is valid.
+    If not, throw an exception.
+    str -> none or error    
+    """
     if type not in get_type_map():
         raise ValueError(type + ' is not a valid type')
 
-# Checks that a given line in the config is valid.
-# If not, throw an exception.
-# str -> none or error
+
 def check_config_line(line):
+    """
+    Checks that a given line in the config is valid.
+    If not, throw an exception.
+    str -> none or error    
+    """
     if len(line.split(' ', 1)) == 1:
         raise ValueError('Invalid config line: ' + line)
 
-# Returns a map of type names to input functions.
-# none -> map of str to (str -> anything)
+
 def get_type_map():
+    """
+    Returns a map of type names to input functions.
+    none -> map of str to (str -> anything)    
+    """
     return {
         'value' : user_enter_value,
         'time' : user_enter_time,
@@ -268,8 +337,11 @@ def get_type_map():
         'state_change' : user_enter_state_change
         }
 
-# Runs the program.
+
 def main():
+    """
+    Runs the program.
+    """
 
     mockConfig = ['value (0,10) stuff", "time Some stuff", "did_do otherStuff']
 
