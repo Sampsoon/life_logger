1aa8a393fcf76f376c4be5795dab3ca578568df1
Name <sampsonJemison@gmail.com>
2019-11-19 20:51:00 -0500

    refactored so that everything uses the user_enter function

--- life_logger.py
+++ life_logger.py
@@ -6,6 +6,50 @@ import constants
 
 # A definition is the text following space after the type name.
 
+
+def user_enter(get_label,
+                user_enter_response, 
+                get_data_from_response,
+                is_valid_data,
+                opening_question_text, 
+                valid_respones_text,
+                definition):
+    """
+    Asks the user to enter data to be logged.
+    Returns the data along with label for the data.
+    Parameters
+    ----------
+    get_label : str -> str
+        gets the data's label from the definition.
+    user_enter_response : nothing -> str
+        Prompts the user to enter a data response.
+    is_valid_data : str -> bool
+        Returns true if the data is valid.
+    opening_question_text : str
+        The text for the question prompted the user the user somthing.
+        This would come with a spot: {} for the data label.
+    valid_respones_text : str
+        The text that define a valid response.
+    definition : str
+        The definition for the data.
+    ----------
+    str -> (label: str, value: int)
+    """
+
+    label = get_label(definition)
+    
+    print(opening_question.format(label))
+    response = user_enter_response()
+
+    while not is_valid_data(response):
+        print('Please enter a valid response')
+        print(valid_respones_text)
+        response = user_enter_response()
+
+    value = get_data_from_response(response)
+
+    return (label, value)
+
 def user_enter_value(definition):
     """
     Get a value from the user.
@@ -15,25 +59,23 @@ def user_enter_value(definition):
     str -> (label: str, value: int)
     """
 
-    label = get_value_label(definition)
     number_range = get_value_range(definition)
     a = number_range[0]
     b = number_range[1]
 
     inclusive_exclusive = 'From a scale from {} inclusive to {} exclusive, '.format(a,b)
-    question = inclusive_exclusive + 'how would you rate your {}'.format(label)
-    print(question)
-    response = user_enter_value_response()
+    question = inclusive_exclusive + 'how would you rate your {}'
 
-    while not is_valid_value(response, a, b):
-        print('Please enter a valid response')
-        print('A valid response is a integer ' + inclusive_exclusive.lower())
-        response = user_enter_value_response()
-
-    value = get_value_from_response(response)
-
-    return (label, value)
-
+    valid = 'A valid response is a integer ' + inclusive_exclusive.lower()
+    
+    is_valid = lambda response: is_valid_value(response, a, b)
+    
+    return user_enter(get_value_label, 
+                      user_enter_value_response, 
+                      is_valid,
+                      question,
+                      valid,
+                      definition)
 
 def user_enter_value_response():
     """
@@ -91,19 +133,16 @@ def user_enter_time(definition):
     Signature in config: time label
     str -> (label: str, value: float)
     """
-    label = get_time_label(definition)
     
-    print('How long did you {}?'.format(label))
-    response = user_enter_time()
-    
-    while not is_valid_time_response(response):
-        print('Please enter a valid response')
-        print('A valid response is a number')
-        response = user_enter_time()
-    
-    time = get_time_from_response(response)
-    
-    return (label, time)
+    question = 'How long did you {}?'
+    valid = 'A valid response is a number'
+
+    return user_enter(get_time_label, 
+                      user_enter_time, 
+                      is_valid_time_response,
+                      question,
+                      valid,
+                      definition)
 
 
 def get_time_from_response(response):
@@ -147,20 +186,16 @@ def user_enter_did_do(definition):
     Signature in config: did_do label
     str -> (label: str, value: bool)
     """
+    
+    question = 'Did you {}?'
+    valid = 'A valid response is eather "y" for True or "n" for False.'
 
-    label = get_did_do_label(definition)
-
-    print('Did you {}?'.format(label))
-    response = user_enter_boolean_response()
-
-    while not is_valid_boolean_response(response):
-        print('Please enter a valid response')
-        print('A valid response is eather "y" for True or "n" for False.')
-        response = user_enter_boolean_response()
-
-    bool_response = get_bool_from_response(response)
-
-    return (label, bool_response)
+    return user_enter(get_did_do_label, 
+                      user_enter_boolean_response, 
+                      is_valid_boolean_response,
+                      question,
+                      valid,
+                      definition)
 
 
 def user_enter_boolean_response():
@@ -202,19 +237,16 @@ def user_enter_note(definition):
     Signature in config: note label
     str -> (label: str, value: str)
     """
-    label = get_note_label(definition)
     
-    print('What notes if any do you have for {}?'.format(label))
-    response = user_enter_note()
-    
-    while not is_valid_note_response(response):
-        print('Please enter a valid response')
-        print('A valid response is a anything...I don't know how you got here')
-        response = user_enter_note()
-    
-    note = get_note_from_response(response)
-    
-    return (label, note)
+    question = 'What notes if any do you have for {}?'
+    valid = 'A valid response is a anything...I don\'t know how you got here'
+
+    return user_enter(get_note_label, 
+                      user_enter_note, 
+                      is_valid_note_response,
+                      question,
+                      valid,
+                      definition)
 
 
 def get_note_from_response(response):
@@ -245,61 +277,9 @@ def get_note_label(definition):
     """
     return definition
 
-#------------------------------------------------------------------------------------
 
 def user_enter_key_event(definition):
-    """
-    Gets a key_event from the user.
-    Returns it along with label for the value.
-    A key_event is a str.
-    Signature in config: key_event label
-    str -> (label: str, value: str)
-    """
-    label = get_note_label(definition)
-    
-    print('What notes if any do you have for {}?'.format(label))
-    response = user_enter_note()
-    
-    while not is_valid_note_response(response):
-        print('Please enter a valid response')
-        print('A valid response is a anything...I don't know how you got here')
-        response = user_enter_note()
-    
-    note = get_note_from_response(response)
-    
-    return (label, note)
-
-
-def get_note_from_response(response):
-    """
-    Gets the time from a valid time response.
-    str -> float
-    """
-    return float(response)
-
-def is_valid_note_response(response):
-    """
-    Returns true if a note response is valid.
-    str -> bool
-    """
-    return True
-
-def user_enter_note():
-    """
-    Prompts the user to enter a note response.
-    nothing -> str
-    """
-    return input('>> ')
-    
-def get_note_label(definition):
-    """
-    Given a valid note definition, returns its label.
-    str -> str
-    """
-    return definition
-
-
-#------------------------------------------------------------------------------------
+    pass
 
 def user_enter_state_change(definition):
     """
